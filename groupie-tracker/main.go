38c3f57cb08fc5752108a5cc0366033d83704ab1
Aukryx <thomaslambert9976@gmail.com>
2024-09-20 10:33:37 +0200

    Add files via upload

---
+++ groupie-tracker/main.go
@@ -0,0 +1,16 @@
+package to_delete
+
+// func formatLocation(locationStr string) string {
+// 	regionAndCountry := strings.Split(locationStr, "-")
+// 	regionWords := strings.Split(regionAndCountry[0], "_")
+// 	for i := range regionWords {
+// 		regionWords[i] = strings.ToUpper(regionWords[i][:1]) + strings.ToLower(regionWords[i][1:])
+// 	}
+// 	region := strings.Join(regionWords, "-")
+// 	countryWords := strings.Split(regionAndCountry[1], "_")
+// 	for i := range countryWords {
+// 		countryWords[i] = strings.ToUpper(countryWords[i][:1]) + strings.ToLower(countryWords[i][1:])
+// 	}
+// 	country := strings.Join(countryWords, " ")
+// 	return fmt.Sprintf("%s (%s)", region, country)
+// }
